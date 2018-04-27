07bbbef9016e9e6307bcc1e4452700cc397ee562
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2018-04-27 15:16:10 +0300

    Magento2 support added.
    - Generage class command
    - Insert namespace
    - Insert class name

--- Magento.py
+++ Magento.py
@@ -17,11 +17,28 @@ class InsertIfIpCommand(sublime_plugin.TextCommand):
             else:
                 self.view.run_command('insert_snippet', {'contents': template % (ip)})
 
+class GenerateClassCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        namespaceDetector = NamespaceDetector(self.view.file_name())
+        classnameDetector = ClassNameDetector(self.view.file_name())
+        template = "namespace %s;\n\nclass %s extends $1\n{\n    $2\n}"
+        self.view.run_command('insert_snippet', {
+            'contents': template % (
+                namespaceDetector.getNamespace(),
+                classnameDetector.getClassName()
+            )
+        })
+
 class InsertClassNameCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         detector = ClassNameDetector(self.view.file_name())
-        for region in self.view.sel():
-            if not region.empty():
-                self.view.replace(edit, region, detector.getClassName())
-            else:
-                self.view.insert(edit, region.begin(), detector.getClassName())
+        self.view.run_command('insert_snippet', {
+            'contents': "${1:%s}" % detector.getClassName()
+        })
+
+class InsertNamespaceCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        detector = NamespaceDetector(self.view.file_name())
+        self.view.run_command('insert_snippet', {
+            'contents': "${1:%s}" % detector.getNamespace()
+        })
